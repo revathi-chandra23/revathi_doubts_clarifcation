4b4334c0e404091f1fb5b358facde2dfb7340779
Revathi chandra <revathichandra2302@gmail.com>
2026-04-05 19:59:35 +0530

    polymorphism

---
+++ src/java/com.java/practice_doubts/polymorphism.java
@@ -0,0 +1,15 @@
+class Animal {
+    void sound() { System.out.println("Animal makes sound"); }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() { System.out.println("Dog barks"); }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.sound(); // Dog barks
+    }
+}
