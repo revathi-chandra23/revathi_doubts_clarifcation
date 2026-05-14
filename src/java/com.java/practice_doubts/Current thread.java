da1f3e43310961d68fd043b40d7a8412892af65f
revathi-chandra23 <revathichandra2302@gmail.com>
2026-05-14 11:00:34 +0530

    running just example

--- src/java/com.java/practice_doubts/Current thread.java
+++
@@ -1,7 +0,0 @@
-public class CurrentThreadExample {
-    public static void main(String[] args) {
-        Thread t = Thread.currentThread(); // currentThread()
-        System.out.println("Current thread name: " + t.getName());
-        System.out.println("Current thread ID: " + t.getId());
-    }
-}
---
+++ src/java/com.java/practice_doubts/running.java
@@ -0,0 +1,5 @@
+public class running{
+    public static void main(String[] args){
+        System.out.println("hello i am running");
+    }
+}
