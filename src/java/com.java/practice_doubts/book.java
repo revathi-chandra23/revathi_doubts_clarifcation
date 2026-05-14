4bad758d52182c659b2fc259410afc4770bc141d
revathi-chandra23 <revathichandra2302@gmail.com>
2026-05-14 12:01:56 +0530

    oops

--- src/java/com.java/practice_doubts/book.java
+++ src/java/com.java/practice_doubts/book.java
@@ -1,3 +1,17 @@
 public class book {
+    public static void main(String[] args) {
+        book b = new book();
+        b.m2();
+        b.m1();
+    }
+
+    public void m2() {
+        System.out.println("hello i am book");
+        }
+        public static void m1(){
+            System.out.println("hello i am static m1");
+        }
+    }
+   
     
-}
+
