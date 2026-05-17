df008a64e358c6f8cd9f668247b77bc0d77e017b
revathi-chandra23 <revathichandra2302@gmail.com>
2026-05-17 13:52:31 +0530

    stream examples

---
+++ src/java/com.java/practice_doubts/streams_examples/axscendingorder.java
@@ -0,0 +1,12 @@
+package streams_examples;
+
+public class axscendingorder {
+    public static void main(String[] args) {
+         int[] arr = {4,5,6,7,1,2,3};
+         Arrays.sort(arr);
+         for(int i:arr){
+              System.out.println(i);
+         }
+    }
+    
+}
